f03f76703b11876e5f4f19fea859c9a82308203f
Julianna Mariano <juliannamariano27@outlook.com>
2025-04-25 10:17:55 -0300

    Criando as entidades status e pedido

---
+++ ms-pedido/src/main/java/entities/Status.java
@@ -0,0 +1,15 @@
+package entities;
+
+public enum Status {
+
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMADO,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    CANCELADO
+
+
+}
