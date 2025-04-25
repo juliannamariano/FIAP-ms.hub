e9e63292a4204b7ef4d2e5f3969fe04c28420fe4
Julianna Mariano <juliannamariano27@outlook.com>
2025-04-25 10:58:10 -0300

    criando o import.sql para testar no banco de dados

---
+++ ms-pedido/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_pedido(nome, cpf, data, status) VALUES ('Jon Snow','12345678935','2025-04-25', 'REALIZADO');
+INSERT INTO tb_pedido(nome, cpf, data, status) VALUES ('Ayra Stark','36547812396','2025-04-25', 'REALIZADO');
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (2, 'Mouse sem fio Microsoft', 250.0, 1);
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (1, 'Teclado sem fio Microsoft', 290.0, 1);
+INSERT INTO tb_item_do_pedido(quantidade, descricao, valor_unitario, pedido_id) VALUES (1, 'Smart TV LG led', 3599.0, 2);
