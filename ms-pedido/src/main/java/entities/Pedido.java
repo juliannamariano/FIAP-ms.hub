9cf4d9e7b9556941a22ab71a0e88bcdc8e01b2d8
Julianna Mariano <juliannamariano27@outlook.com>
2025-04-25 10:42:07 -0300

    Adicionando relacionamento nas entidades e @Column

--- ms-pedido/src/main/java/entities/Pedido.java
+++ ms-pedido/src/main/java/entities/Pedido.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,11 +20,17 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome", nullable = false, length = 10)
     private String nome;
+    @Column(nullable = false, length = 11)
     private String cpf;
     private LocalDate data;
 
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    // Relacionamento
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.PERSIST)
+    private List<ItemDoPedido> items = new ArrayList<>();
 
 }
